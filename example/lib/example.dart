345020c25dc45881354f22c1a466f587820a3aad
Juan Diego Fajardo <jfajardo@mit.edu>
2019-06-11 01:22:11 +0000

    Add examples for all generators.

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -3,7 +3,7 @@ import 'package:copyable_generator/annotations.dart';
 
 part 'example.g.dart';
 
-@copy_functions
+/// Naive implementation of Copyable
 class Point implements Copyable<Point> {
   final int x;
   final int y;
@@ -22,15 +22,66 @@ class Point implements Copyable<Point> {
   Point copyFrom(Point master) => _copy(master);
 
   @override
-  Point copyWith({int x, int y, Point parent}) => _copy(null, x: x, y: y, parent: parent);
+  Point copyWith({
+    int x,
+    int y, 
+    Point parent
+  }) => _copy(null, 
+      x: x, 
+      y: y, 
+      parent: parent
+  );
 
-  Point _copy(Point master, {int x, int y, Point parent}) {
-    return Point(x : x ?? master?.x, y : y ?? master?.y, parent : parent ?? master?.parent);
+  Point _copy(Point master, {
+    int x, 
+    int y, 
+    Point parent
+  }) {
+    return Point(
+        x : x ?? master?.x, 
+        y : y ?? master?.y, 
+        parent : parent ?? master?.parent
+    );
   }
 }
 
+/// Use generator to write copy code for you! Just add the @copy_functions
+/// annotation to your class. Don't forget to add `implements
+/// Copyable<{className}>`
+@copy_functions
+class Rectangle implements Copyable<Rectangle> {
+  final int length;
+  final int width;
+  Rectangle parent;
+
+  Rectangle({
+    this.length,
+    this.width,
+    this.parent
+  });
+
+  // Paste code from example.g.dart here:
+}
+
+/// Use the @build_copier annotation to generate a Copier for your class.
+@build_copier
+class Circle {
+  final int radius;
+  final int centerX;
+  final int centerY;
+
+  Circle({
+    this.radius,
+    this.centerX,
+    this.centerY
+  });
+}
+
+/// For classes that are defined elsewhere (not editable, i.e. Flutter
+/// widgets), create a CopyMeta object describing the object. The generator
+/// will then create a Copier for that object.
 const CopyMeta pointCopyMeta = CopyMeta(
-  baseClass: 'Point',
+  baseClass: 'ExternalPoint',
   fields: {
     'x': 'int',
     'y': 'int',
--- example/lib/example.g.dart
+++ example/lib/example.g.dart
@@ -3,6 +3,66 @@
 part of 'example.dart';
 
 // **************************************************************************
+// CopierGenerator
+// **************************************************************************
+
+class CircleCopier implements Copier<Circle> {
+  CircleCopier([this.master]);
+
+  Circle master;
+
+  Circle get defaultMaster {
+    // TODO: Implement default
+    return null;
+  }
+
+  dynamic internalCopy(Circle master,
+      {bool resolve = false, int radius, int centerX, int centerY}) {
+    master = master ?? this.master;
+    Circle newCircle = Circle(
+        radius: radius ?? master?.radius ?? defaultMaster.radius,
+        centerX: centerX ?? master?.centerX ?? defaultMaster.centerX,
+        centerY: centerY ?? master?.centerY ?? defaultMaster.centerY);
+
+    return resolve ? newCircle : CircleCopier(newCircle);
+  }
+
+  CircleCopier copy(Circle master) {
+    return this.internalCopy(
+      master,
+      resolve: false,
+    ) as CircleCopier;
+  }
+
+  Circle copyAndResolve(Circle master) {
+    return this.internalCopy(
+      master,
+      resolve: true,
+    ) as Circle;
+  }
+
+  CircleCopier copyWith({int radius, int centerX, int centerY}) {
+    return this.internalCopy(this.master,
+        resolve: false,
+        radius: radius,
+        centerX: centerX,
+        centerY: centerY) as CircleCopier;
+  }
+
+  Circle copyWithAndResolve({int radius, int centerX, int centerY}) {
+    return this.internalCopy(this.master,
+        resolve: true,
+        radius: radius,
+        centerX: centerX,
+        centerY: centerY) as Circle;
+  }
+
+  Circle resolve() {
+    return this.master;
+  }
+}
+
+// **************************************************************************
 // CopyableGenerator
 // **************************************************************************
 
@@ -39,17 +99,18 @@ class CopyablePoint extends Point implements Copyable<Point> {
 // **************************************************************************
 
 /*
-      @override
-      Point copy() => _copy(this);
+    Copy/Paste these methods into your class! Make sure to remember to 
+          @override
+      Rectangle copy() => _copy(this);
     
       @override
-      Point copyFrom(Point master) => _copy(master);
+      Rectangle copyFrom(Rectangle master) => _copy(master);
     
       @override
-      Point copyWith({int x, int y, Point parent}) => _copy(null, x: x, y: y, parent: parent);
+      Rectangle copyWith({int length, int width, Rectangle parent}) => _copy(null, length: length, width: width, parent: parent);
     
-      @override
-      Point _copy(Point master, {int x, int y, Point parent}) {
-        return Point(x : x ?? master?.x, y : y ?? master?.y, parent : parent ?? master?.parent);
+      Rectangle _copy(Rectangle master, {int length, int width, Rectangle parent}) {
+        return Rectangle(length : length ?? master?.length, width : width ?? master?.width, parent : parent ?? master?.parent);
       }
+    
     */
